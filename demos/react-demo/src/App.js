4af851d90cf4a0f8db781063aff7ddc1404d328d
yujianbohz <yujianbosd@163.com>
2020-05-26 15:24:05 +0800

    [CHG]修改demo中ezuikit 版本号

--- demos/react-demo/src/App.js
+++ demos/react-demo/src/App.js
@@ -20,6 +20,7 @@ class App extends React.Component{
       plugin: ['talk'],                       // 加载插件，talk-对讲
       // 视频下方底部控件
       footer: ['talk','broadcast','hd','fullScreen'],      // 如果template参数不为simple,该字段将被覆盖
+      audio: 1, // 是否默认开启声音 0 - 关闭 1 - 开启
       openSoundCallBack: (data) => console.log("开启声音回调",data),
       closeSoundCallBack: (data) => console.log("关闭声音回调",data),
       startSaveCallBack: (data) => console.log("开始录像回调",data),
