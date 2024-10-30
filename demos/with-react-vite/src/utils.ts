36eeb18a90654154f9d61ce446bf56c50db57793
ShineShao <xiaoshaoqq@gmail.com>
2024-10-31 00:06:42 +0800

    fix: fixed

--- demos/with-react-vite/src/utils.ts
+++ demos/with-react-vite/src/utils.ts
@@ -1,5 +1,5 @@
 export function isMobile() {
-  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  const userAgent = navigator.userAgent || navigator.vendor;
   return /android|iPhone|iPad|iPod|blackberry|windows phone|mobile/i.test(
     userAgent
   );
