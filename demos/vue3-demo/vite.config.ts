17db6aa539e8f6406c4be05b41c942c1512e9bab
ShineShao <xiaoshaoqq@gmail.com>
2024-12-17 19:42:47 +0800

    demo: update ezuikit-js@8.1.2

--- demos/vue3-demo/vite.config.ts
+++ demos/vue3-demo/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  build: {
+    minify: "terser",
+    terserOptions: {
+      // 清除console和debugger
+      compress: {
+        // drop_console 暂时不能用，会报错
+        // https://github.com/Ezviz-OpenBiz/EZUIKit-JavaScript-npm/issues/138
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
+  plugins: [vue()],
+});
