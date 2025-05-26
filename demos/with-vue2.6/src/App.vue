2997df4c073fb93a6a05862246db2a74a6c4fc50
ShineShao <xiaoshaoqq@gmail.com>
2025-05-26 21:11:34 +0800

    demo: add vue2.6 demo

---
+++ demos/with-vue2.6/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app"></div>
+</template>
+<script>
+import Player from "./components/Player.vue";
+
+export default {
+  name: "App",
+  components: {
+    Player,
+  },
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
