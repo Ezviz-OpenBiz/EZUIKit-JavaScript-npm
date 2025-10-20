ad78a727b7d11deeae549f26162e5926dfcfebbb
ShineShao <xiaoshaoqq@gmail.com>
2025-10-20 19:49:58 +0800

    feat: update

--- demos/with-vue2.6/vue.config.js
+++ demos/with-vue2.6/vue.config.js
@@ -1,17 +1,56 @@
 // vue.config.js
+// module.exports = {
+//   // 该对象将会被 webpack-merge 合并入最终的 webpack 配置。
+//   // 文档：https://cli.vuejs.org/zh/guide/webpack.html
+//   configureWebpack: {
+//     resolve: {
+//       extensions: ['.js', '.vue', '.json', '.mjs'],
+//     },
+//     module: {
+//       rules: [
+//         // 支持 node_modules 下 mjs
+//         {
+//           test: /\.mjs$/,
+//           include: /node_modules/,
+//           type: "javascript/auto",
+//         },
+//       ],
+//     },
+//   },
+//   chainWebpack: config => {
+//     config.module
+//       .rule('mjs')
+//       .test(/\.mjs$/)
+//       .include
+//         .add(/node_modules/)
+//         .end()
+//       .type('javascript/auto');
+//   }
+// };
+
+// vue.config.js
 module.exports = {
-  // 该对象将会被 webpack-merge 合并入最终的 webpack 配置。
-  // 文档：https://cli.vuejs.org/zh/guide/webpack.html
   configureWebpack: {
+    resolve: {
+      extensions: ['.js', '.vue', '.json', '.mjs'],
+    },
     module: {
       rules: [
-        // 支持 node_modules 下 mjs
         {
           test: /\.mjs$/,
           include: /node_modules/,
-          type: "javascript/auto",
-        },
-      ],
-    },
+          type: 'javascript/auto'
+        }
+      ]
+    }
   },
-};
+  chainWebpack: config => {
+    config.module
+      .rule('mjs')
+      .test(/\.mjs$/)
+      .include
+        .add(/node_modules/)
+        .end()
+      .type('javascript/auto');
+  }
+}
